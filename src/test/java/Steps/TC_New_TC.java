b0b4998d7f600e8ce08ed2707cc563fd334c83ee
Vaishnavi <vaishitlead@gmail.com>
2021-05-26 00:35:13 +0530

    Added TC002 and modified new_TC

--- src/test/java/Steps/TC_New_TC.java
+++ src/test/java/Steps/TC_New_TC.java
@@ -1,2 +1,8 @@
 //this is new test case
 //this TC will check the customer
+
+
+//this latest one..code added newly
+
+//............
+
---
+++ src/test/java/Steps/TC_New_TC002.java
@@ -0,0 +1,5 @@
+package Steps;
+
+public class TC_New_TC002 {
+
+}
